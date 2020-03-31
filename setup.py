2877f04dbdfa2958dcf765ca3aeb8b87728852e7
saudzi <saudzi@ttu.ee>
2020-03-31 19:55:27 +0300

    fixed url in cfg

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'quadratic-equation-saudzi',         
   packages = ['quadratic-equation-saudzi'],   
-  version = '0.1',      
+  version = '0.2',      
   license='MIT',       
   description = 'just a simple tool to solve quadratic equations',  
   author = 'saba udzilauri',                  
   author_email = 'sabaxbox1@gmail.com',     
   url = 'https://github.com/saudzi',  
-  download_url = 'https://github.com/saudzi/quadratic-equation-saudzi/archive/0.1.tar.gz',   
+  download_url = 'https://github.com/saudzi/quadratic-equation-saudzi/archive/0.2.tar.gz',   
   keywords = ['quadratic''quadeq''equation'],  
   install_requires=[            
           'cmath',
